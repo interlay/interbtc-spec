01c9d1ceb92d3f31ed292e9f0d07bb8d7e5adda0
alexei.zamyatin <alexei@interlay.io>
2020-01-12 22:57:34 +0000

    added btc relay spec to gitlab pages

--- btcrelay-spec/docs/source/conf.py
+++ btcrelay-spec/docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2020, Interlay'
 author = u'Interlay'
 
 # The short X.Y version
-version = u''
+version = u'v1.0'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'v1.0'
 
 
 # -- General configuration ---------------------------------------------------
