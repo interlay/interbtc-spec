e0470266e5706f504c569e87aec1c0e5e016f1e5
alexei.zamyatin <alexei@interlay.io>
2020-02-25 22:11:45 +0100

    bumbed spec version to v1.1. in conf.py

--- polkabtc-spec/docs/source/conf.py
+++ polkabtc-spec/docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Interlay'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'1.1'
 
 
 # -- General configuration ---------------------------------------------------
