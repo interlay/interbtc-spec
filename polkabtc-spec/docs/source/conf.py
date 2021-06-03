534104cdf8441b5e6a410665d5653e4b6be9bd0c
Dominik Harz <dominik@interlay.io>
2021-06-03 11:14:15 +0200

    chore: bump version to 0.3.3

--- polkabtc-spec/docs/source/conf.py
+++ polkabtc-spec/docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2021, Interlay'
 author = u'Interlay'
 
 # The short X.Y version
-version = u'v3.2.0'
+version = u'v3.3.0'
 # The full version, including alpha/beta/rc tags
-release = u'v3.2.0'
+release = u'v3.3.0'
 
 
 # -- General configuration ---------------------------------------------------
